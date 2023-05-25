45d34d542b1c5f138f7cb65f82c39d29357371f4
XHHYYY <3197086058@qq.com>
2023-05-25 13:32:47 +0800

    finishe

--- codes/generate_sequence.py
+++ codes/generate_sequence.py
@@ -1,9 +1,9 @@
 import random
 txt = ''
-m = 10
+m = 50
 max_arrive = 0
 for i in range(1, m+1):
-        max_arrive = max_arrive + random.randint(1, 5)
+        max_arrive = max_arrive + random.randint(1, 3)
         require_time = random.randint(1, 20)
         txt += '{} {} {}\n'.format(*list(map(str, [i, max_arrive, require_time])))
 print(txt)
